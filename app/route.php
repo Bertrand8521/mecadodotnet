d2f144643773be68e805c0c6699e3417550a6bca
Bertrand <bertrandpenguilly@laposte.net>
2017-11-07 13:45:17 +0100

    new route

--- app/route.php
+++ app/route.php
@@ -8,3 +8,5 @@ $app->get('/', 'ExempleController:exemple')->setName('index');
 $app->get('/userRegister', 'UserController:registerUser')->setName('register');
 
 $app->get('/item', 'ItemController:item');
+
+$app->get('/item/{liste_id}', 'ItemController:getItemsFromListeId');
