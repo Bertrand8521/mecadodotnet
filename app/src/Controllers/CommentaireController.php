1e61532265b64a7a598f874cfb04eea5776f5d22
valentin <valentin.escamilla@outlook.fr>
2017-11-08 10:42:11 +0100

    Add getters for commentaires

--- app/src/Controllers/CommentaireController.php
+++ app/src/Controllers/CommentaireController.php
@@ -9,7 +9,17 @@ use App/Models/Commentaire_liste.php
 use App/Models/Commentaire_item.php
 
 final class CommentaireController extends BaseController {
-	public function getCommentaire(Request $request, Response $response, $args) {
-		
+	public function getCommentaireItem(Request $request, Response $response, $args) {
+
+		$commentaireitem = Commentaire_item::where('id', '=', $args['commentaire_id'])->get()->toArray();
+		return $this->container->view->render($response, "commentaireitem.twig", [commentaireitem => $commentaireitem]);
+
+	}
+
+	public function getCommentaireListe(Request $request, Response $response, $args) {
+
+		$commentaireliste = Commentaire_liste::where('id', '=', $args['commentaire_id'])->get()->toArray();
+		return $this->container->view->render($response, "commentaireliste.twig", [commentaireliste => $commentaireliste]); // Probable de devoir remplacer par le fichier twig une fois qu'il sera créé
+
 	}
 }
