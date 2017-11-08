9cf87c602317a135c0c834bf6ba640b748c8c277
valentin <valentin.escamilla@outlook.fr>
2017-11-08 10:20:20 +0100

    Add CommentaireController

---
+++ app/src/Controllers/CommentaireController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+use App/Models/Commentaire_liste.php
+use App/Models/Commentaire_item.php
+
+final class CommentaireController extends BaseController {
+	public function getCommentaire(Request $request, Response $response, $args) {
+		
+	}
+}
