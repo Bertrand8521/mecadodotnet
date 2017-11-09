d7533f5cae88911cf7f27b2c8f99133f8b3a4e06
Bertrand <bertrandpenguilly@laposte.net>
2017-11-09 09:05:13 +0100

    Add comment count on each list

--- app/src/Controllers/CommentaireController.php
+++ app/src/Controllers/CommentaireController.php
@@ -5,21 +5,28 @@ namespace App\Controllers;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
-use App/Models/Commentaire_liste.php
-use App/Models/Commentaire_item.php
+use App\Models\Commentaire_liste;
+use App\Models\Commentaire_item;
 
 final class CommentaireController extends BaseController {
-	public function getCommentaireItem(Request $request, Response $response, $args) {
+	public function getCommentaireItem($id) {
 
-		$commentaireitem = Commentaire_item::where('id', '=', $args['commentaire_id'])->get()->toArray();
-		return $this->container->view->render($response, "commentaireitem.twig", [commentaireitem => $commentaireitem]);
+		return Commentaire_item::where('item_id', '=', $id)->get()->toArray();
+		// $commentaireitem = Commentaire_item::where('id', '=', $args['commentaire_id'])->get()->toArray();
+		// return $this->container->view->render($response, "commentaireitem.twig", [commentaireitem => $commentaireitem]);
 
 	}
 
-	public function getCommentaireListe(Request $request, Response $response, $args) {
+	public function getCommentaireListe($id) {
 
-		$commentaireliste = Commentaire_liste::where('id', '=', $args['commentaire_id'])->get()->toArray();
-		return $this->container->view->render($response, "commentaireliste.twig", [commentaireliste => $commentaireliste]); // Probable de devoir remplacer par le fichier twig une fois qu'il sera créé
+		return Commentaire_liste::where('liste_id', '=', $id)->get()->toArray();
+		// $commentaireliste = Commentaire_liste::where('id', '=', $args['commentaire_id'])->get()->toArray();
+		// return $this->container->view->render($response, "commentaireliste.twig", [commentaireliste => $commentaireliste]); // Probable de devoir remplacer par le fichier twig une fois qu'il sera créé
 
+	}
+
+	public function nbCommentaireListe($id) {
+
+		return Commentaire_liste::where('liste_id', '=', $id)->count();
 	}
 }
--- app/src/Controllers/ShowListsController.php
+++ app/src/Controllers/ShowListsController.php
@@ -8,6 +8,8 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Slim\Flash\Messages;
 use App\Models\Liste;
 
+use App\Controllers\CommentaireController;
+
 final class ShowListsController extends BaseController
 {
 
@@ -15,9 +17,15 @@ final class ShowListsController extends BaseController
     public function showlists(Request $request, Response $response, $args)
     {
         $createur_id = $_SESSION['id'];
-        $listes = Liste::where('createur_id', '=', $createur_id)->get()->toArray();
+        $listes_query = Liste::where('createur_id', '=', $createur_id)->get();
 
-        return $this->container->view->render($response, 'showlists.twig', [listes => $listes]);
+        $listes = $listes_query->toArray();
+
+        $nbCommentaires = []; // liste de nombre de commnetaires (dans le meme ordre que les listes)
+        $listes_query->map(function ($liste) use (&$nbCommentaires) {
+          $nbCommentaires[] = CommentaireController::nbCommentaireListe($liste->id);
+        });
+        return $this->container->view->render($response, 'showlists.twig', ['listes' => $listes, 'nbCommentaires' => $nbCommentaires]);
     }
 
  }
